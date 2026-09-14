cb833681dbff91f23e4fa854020b0dbffcb8b81c
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-14 10:08:21 +0200

    feat: create login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import LoginSerializer
+
+
+class LoginView(APIView):
+
+    def post(self, request):
+        serializer = LoginSerializer(
+            data=request.data
+        )
+
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+
+        return Response(
+            {
+                "message": "Login successful.",
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                    "email": user.email,
+                    "role": user.role,
+                },
+            },
+            status=status.HTTP_200_OK,
+        )
