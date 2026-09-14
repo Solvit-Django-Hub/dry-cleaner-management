e99845169365508f42b0e91081cf6a2bb7091b38
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-14 14:41:45 +0200

    feat: add JWT token endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .views import LoginView, RegistrationView
 
@@ -9,5 +13,15 @@ urlpatterns = [
         "register/",
         RegistrationView.as_view(),
         name="register",
+    ),
+    path(
+        "token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
     ),
 ]
