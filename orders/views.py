f98e22f163ac9c6909b4be601f5e07125b154373
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-15 12:04:18 +0200

    add customer permission

--- orders/views.py
+++ orders/views.py
@@ -6,33 +6,54 @@ from .serializers import (
     OrderItemSerializer,
     OrderSerializer,
 )
+from rest_framework.permissions import IsAuthenticated
+from accounts.permissions import IsAdminOrStaff
 
+permission_classes = [IsAuthenticated]
 
 class ClothingItemListCreateView(generics.ListCreateAPIView):
     queryset = ClothingItem.objects.all()
     serializer_class = ClothingItemSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class ClothingItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = ClothingItem.objects.all()
     serializer_class = ClothingItemSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class OrderDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class OrderItemListCreateView(generics.ListCreateAPIView):
-
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticated]
+
 
 class OrderItemDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = OrderItem.objects.all()
-    serializer_class = OrderItemSerializer 
+    serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ClothingItemListCreateView(generics.ListCreateAPIView):
+    queryset = ClothingItem.objects.all()
+    serializer_class = ClothingItemSerializer
+    permission_classes = [IsAdminOrStaff]
+
+
+class ClothingItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ClothingItem.objects.all()
+    serializer_class = ClothingItemSerializer
+    permission_classes = [IsAdminOrStaff]
