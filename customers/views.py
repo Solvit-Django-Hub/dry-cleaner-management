b1234410c156a038ba36c49d24310dd8712b9d43
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-15 14:14:29 +0200

    filter customers by ownership

--- customers/views.py
+++ customers/views.py
@@ -5,13 +5,21 @@ from accounts.permissions import IsOwnerOrAdminStaff
 from .models import Customer
 from .serializers import CustomerSerializer
 
+
 class CustomerListCreateView(generics.ListCreateAPIView):
-    queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role in ["ADMIN", "STAFF"]:
+            return Customer.objects.all()
+
+        return Customer.objects.filter(user=user)
+
 
 class CustomerDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAuthenticated, IsOwnerOrAdminStaff]
-
