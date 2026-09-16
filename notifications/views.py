15f981ed131551c2fe2f99c9b6118ab7bd543d40
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-16 13:24:03 +0200

    restrict notification access

--- notifications/views.py
+++ notifications/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 
-from accounts.permissions import IsNotificationOwnerOrAdminStaff
+from accounts.permissions import (
+    IsAdminStaffOrReadOnlyCustomer,
+    IsNotificationOwnerOrAdminStaff,
+)
+
 from .models import Notification
 from .serializers import NotificationSerializer
 
 
 class NotificationListCreateView(generics.ListCreateAPIView):
     serializer_class = NotificationSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminStaffOrReadOnlyCustomer]
 
     def get_queryset(self):
         user = self.request.user
@@ -16,13 +19,15 @@ class NotificationListCreateView(generics.ListCreateAPIView):
         if user.role in ["ADMIN", "STAFF"]:
             return Notification.objects.all()
 
-        return Notification.objects.filter(customer__user=user)
+        return Notification.objects.filter(
+            customer__user=user
+        )
 
 
 class NotificationDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
     permission_classes = [
-        IsAuthenticated,
+        IsAdminStaffOrReadOnlyCustomer,
         IsNotificationOwnerOrAdminStaff,
     ]
