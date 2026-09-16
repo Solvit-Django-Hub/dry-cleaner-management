4b704b1e97604c6f15e008c07dbc785c482e4755
marleneuwamahoro <marleneuwamahoro422@gmail.com>
2026-09-16 13:23:23 +0200

    restrict delivery access

--- deliveries/views.py
+++ deliveries/views.py
@@ -1,14 +1,17 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
 
-from accounts.permissions import IsDeliveryOwnerOrAdminStaff
+from accounts.permissions import (
+    IsAdminStaffOrReadOnlyCustomer,
+    IsDeliveryOwnerOrAdminStaff,
+)
+
 from .models import Delivery
 from .serializers import DeliverySerializer
 
 
 class DeliveryListCreateView(generics.ListCreateAPIView):
     serializer_class = DeliverySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminStaffOrReadOnlyCustomer]
 
     def get_queryset(self):
         user = self.request.user
@@ -16,13 +19,15 @@ class DeliveryListCreateView(generics.ListCreateAPIView):
         if user.role in ["ADMIN", "STAFF"]:
             return Delivery.objects.all()
 
-        return Delivery.objects.filter(order__customer__user=user)
+        return Delivery.objects.filter(
+            order__customer__user=user
+        )
 
 
 class DeliveryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Delivery.objects.all()
     serializer_class = DeliverySerializer
     permission_classes = [
-        IsAuthenticated,
+        IsAdminStaffOrReadOnlyCustomer,
         IsDeliveryOwnerOrAdminStaff,
     ]
